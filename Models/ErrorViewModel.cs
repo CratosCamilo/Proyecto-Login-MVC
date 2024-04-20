bd21e985b3c980f6fe996c8e85db0f8f0dd3b6ac
Camilo Castro <cratoscamilo@gmail.com>
2024-04-20 18:57:09 -0500

    init Proyecto Login

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Login_MVC_.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
